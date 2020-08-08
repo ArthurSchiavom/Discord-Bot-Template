fcf8db177b3446b6bd607b59ced2a77b15a439a7
Arthur Schiavom <arthur.silva.kon@gmail.com>
2020-08-08 03:55:38 +0100

    renaming

--- src/main/java/arthur/silva/discordbot/base/boot/bootstrap/boostrappers/UserConfigurationBoostrapperConfig.java
+++ src/main/java/arthur/silva/discordbot/base/boot/bootstrap/boostrappers/UserConfigurationBoostrapperConfig.java
@@ -11,27 +11,27 @@ public class UserConfigurationBoostrapperConfig {
     /**
      * Process user configurations.
      *
-     * @param configurationWithLowerCaseNames map where keys are the configuration keys and the values are the respective configuration value
+     * @param configurationNames map where keys are the configuration keys and the values are the respective configuration value. Keys are lowercase and contain no spaces.
      */
-    public static void processUserConfigurations(String configurationFilePath, Map<String, String> configurationWithLowerCaseNames) throws BootstrapException {
+    public static void processUserConfigurations(String configurationFilePath, Map<String, String> configurationNames) throws BootstrapException {
         String selectedConfiguration;
 
-        selectedConfiguration = configurationWithLowerCaseNames                                               .get("token");
+        selectedConfiguration = configurationNames                                               .get("token");
         if (selectedConfiguration == null)
             throw new BootstrapException("The bot token configuration (token=123456789) is missing in the configuration file: " + configurationFilePath, true);
         GlobalConfiguration.Bot.token = selectedConfiguration;
 
-        selectedConfiguration = configurationWithLowerCaseNames                                               .get("commandsprefix");
+        selectedConfiguration = configurationNames                                               .get("commandsprefix");
         if (selectedConfiguration == null)
             throw new BootstrapException("The bot prefix configuration (prefix=\"! \") is missing in the configuration file: " + configurationFilePath, true);
         GlobalConfiguration.Command.commandPrefix = convertQuoteMarkedConfig(selectedConfiguration);
         GlobalConfiguration.Command.commandPrefixNChars = GlobalConfiguration.Command.commandPrefix.length();
 
-        GlobalConfiguration.Bot.playingStatus = configurationWithLowerCaseNames                               .get("game");
-        GlobalConfiguration.Command.defaultEmbedColor = Color.decode(configurationWithLowerCaseNames          .get("helpembedcolor"));
-        GlobalConfiguration.Command.helpEmbedFooterImageUrl = configurationWithLowerCaseNames                 .get("helpembedfooterimageurl");
-        GlobalConfiguration.Command.helpEmbedFooterText = configurationWithLowerCaseNames                     .get("helpembedfootertext");
-        GlobalConfiguration.Command.mainMenuThumbnail = configurationWithLowerCaseNames                       .get("mainmenuthumbnail");
+        GlobalConfiguration.Bot.playingStatus = configurationNames                               .get("game");
+        GlobalConfiguration.Command.defaultEmbedColor = Color.decode(configurationNames          .get("helpembedcolor"));
+        GlobalConfiguration.Command.helpEmbedFooterImageUrl = configurationNames                 .get("helpembedfooterimageurl");
+        GlobalConfiguration.Command.helpEmbedFooterText = configurationNames                     .get("helpembedfootertext");
+        GlobalConfiguration.Command.mainMenuThumbnail = configurationNames                       .get("mainmenuthumbnail");
     }
 
     /**
